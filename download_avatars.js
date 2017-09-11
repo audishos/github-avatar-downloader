383d415c3a0cfa66fcb4849d58e886cd7280136b
audishos <audisho.sada@gmail.com>
2017-09-11 17:16:55 -0400

    Adds option object and prints error, response and body

--- download_avatars.js
+++ download_avatars.js
@@ -4,12 +4,22 @@ const fs = require('fs');
 const GITHUB_USER = 'audishos';
 const GITHUB_TOKEN = '4bb1bbc514fce6c9ade965f65d5b0ade3ebf1b13';
 
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  const requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-  console.log(requestURL);
+  const options = {
+    url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  }
 
+  request(options, function(error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
