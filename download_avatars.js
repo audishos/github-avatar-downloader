72b3df2de22920cc6e280ef44fcd8e37e4c852aa
audishos <audisho.sada@gmail.com>
2017-09-11 18:35:38 -0400

    Cleans up code. Adds comments. Adds console output for download statuses. Adds required avatars foolder.

--- download_avatars.js
+++ download_avatars.js
@@ -9,6 +9,8 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 const REPOOWNER = process.argv[2];
 const REPONAME = process.argv[3];
 
+// gets the contrubutors for the specified repo owner and repo name
+// accepts a callback in order to process the list
 function getRepoContributors(repoOwner, repoName, cb) {
   const options = { // github reuires that a User-Agent is passed so we must use the options object
     url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
@@ -18,33 +20,47 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
 
   request(options, function(error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    if (error) { // display error if true
+      console.log("The following error occurred:", error);
+      return;
+    }
+   if (response && response.statusCode !== 200) { // display response if not 200
+     console.log("The request was not successful. Response code:", response.statusCode);
+     return;
+   }
     cb(null, JSON.parse(body)); // parse body and return it to the callback function
   });
 }
 
+// downloads the image at the specified URL to the specified path
 function downloadImageByURL(url, filePath) {
   request.get(url)
-  .on('error', function(err) {
-    throw err;
+  .on('error', function(error) {
+    console.log("The following error occurred:", error);
   })
   .on('response', function(response) {
-    console.log("Status code:", response && response.statusCode);
-    console.log("Content type:" + response.headers['content-type']);
+    if (response && response.statusCode !== 200) {
+      console.log("The request was not successful. Response code:", response.statusCode);
+    }
+    else {
+      console.log(`Downloading ${url} to ${filePath} ...`);
+    }
+  })
+  .on('end', function() {
+    console.log(`Download of ${url} to ${filePath} is complete!!`);
   })
   .pipe(fs.createWriteStream(filePath));
 
 }
 
 if (REPOOWNER && REPONAME && REPOOWNER !== '' && REPONAME !== '') {
-  getRepoContributors(REPOOWNER, REPONAME, function(err, result) {
-    result.forEach(function(element) {
-      downloadImageByURL(element['avatar_url'], `./avatars/${element['login']}.jpg`);
+  getRepoContributors(REPOOWNER, REPONAME, function(err, result) { // get all contributors for the project
+    result.forEach(function(element) { // iterate through the returned list
+      downloadImageByURL(element['avatar_url'], `./avatars/${element['login']}.jpg`); // download their avatar
     });
   });
 }
-else {
+else { // error message when repo owner and repo name are not supplied
   console.log("Please enter a repository owner and a repository name");
   console.log("Usage:");
   console.log("\tnode download_avatars.js nodejs node");
