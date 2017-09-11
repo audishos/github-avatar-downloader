8c61381eaf73da2906d983553975f0c120540a21
audishos <audisho.sada@gmail.com>
2017-09-11 16:56:07 -0400

    Adds GITHUB variables and builds requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,15 @@
-var request = require('request');
+const request = require('request');
+const fs = require('fs');
+
+const GITHUB_USER = 'audishos';
+const GITHUB_TOKEN = '4bb1bbc514fce6c9ade965f65d5b0ade3ebf1b13';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  const requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  console.log(requestURL);
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
