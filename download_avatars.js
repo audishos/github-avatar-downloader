bafabb9f2873507d45560fd9655335c26e742333
audishos <audisho.sada@gmail.com>
2017-09-11 17:54:23 -0400

    Refactors app to get repo owner and repo name as command line args

--- download_avatars.js
+++ download_avatars.js
@@ -6,6 +6,9 @@ const GITHUB_TOKEN = '4bb1bbc514fce6c9ade965f65d5b0ade3ebf1b13';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+const REPOOWNER = process.argv[2];
+const REPONAME = process.argv[3];
+
 function getRepoContributors(repoOwner, repoName, cb) {
   const options = { // github reuires that a User-Agent is passed so we must use the options object
     url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
@@ -21,12 +24,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  result.forEach(function(element) {
-    downloadImageByURL(element['avatar_url'], `./avatars/${element['login']}.jpg`);
-  });
-});
-
 function downloadImageByURL(url, filePath) {
   request.get(url)
   .on('error', function(err) {
@@ -40,4 +37,15 @@ function downloadImageByURL(url, filePath) {
 
 }
 
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+if (REPOOWNER && REPONAME && REPOOWNER !== '' && REPONAME !== '') {
+  getRepoContributors(REPOOWNER, REPONAME, function(err, result) {
+    result.forEach(function(element) {
+      downloadImageByURL(element['avatar_url'], `./avatars/${element['login']}.jpg`);
+    });
+  });
+}
+else {
+  console.log("Please enter a repository owner and a repository name");
+  console.log("Usage:");
+  console.log("\tnode download_avatars.js nodejs node");
+}
